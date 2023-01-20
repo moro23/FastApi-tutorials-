8c452fa2afa6f12579f9664d64d5f36e012aff3d
moro23 <billhab66@gmail.com>
2023-01-20 17:00:57 +0000

    updated database connection file.

--- blog/database.py
+++ blog/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog .db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 ## creating a connection
