a1726fca35219f3a9535c1036590c75d7c6c3e33
moro23 <billhab66@gmail.com>
2023-01-22 07:21:53 +0000

    added a function to get all blog posts and individual post.

--- blog/main.py
+++ blog/main.py
@@ -1,18 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from .schemas import Blog
+from . import  models, schemas
+from .database import  SessionLocal, engine
 
-
-from . import  models
-from .database import  engine
-
-## migrating the tables in the db
+## migrating the tables in the db 
 models.Base.metadata.create_all(bind=engine)
 
 ## creating an instance of FastAPI
 app = FastAPI()
 
+## creating db connection
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.post('/blog')
-def createBlog(request: Blog):
-    return request 
+def createBlog(request: schemas.Blog, db: Session = Depends(get_db)):
+    """
+    Creating blog post
+    """
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog')
+def getAllBlogPost(db: Session = Depends(get_db)):
+    """
+    List all blog post
+    """
+    all_posts = db.query(models.Blog).all()
+    return all_posts
+
+@app.get('/blog/{id}')
+def getIndividualPost(id,db: Session = Depends(get_db)):
+    """
+    Retrive individual post with a specific id
+    """
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
