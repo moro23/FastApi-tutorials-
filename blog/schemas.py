a984d393631f99a2390802b2bd6f4e61061ecbf3
moro23 <billhab66@gmail.com>
2023-01-23 10:54:19 +0000

    configured the orm model.

--- blog/schemas.py
+++ blog/schemas.py
@@ -3,3 +3,13 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+    
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+
+    class Config():
+        orm_mode = True
+    
