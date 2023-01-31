e3db2feda54a7ba8aa8847e9349d5e53d569ff90
moro23 <billhab66@gmail.com>
2023-01-31 17:14:39 +0000

    added function to get individual users.

--- blog/main.py
+++ blog/main.py
@@ -87,15 +87,25 @@ def updatePost(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 
-@app.post('/user')
+@app.post('/user', response_model=schemas.ShowUser)
 def createUser(request: schemas.User, db: Session = Depends(get_db)):
     """
     Creating a User 
     """
     ## lets hash the password coming from the user
-    hashed_password = pwd_context.hash(request.password)
+    #hashed_password = pwd_context.hash(request.password)
     new_user = models.User(name=request.name, email=request.email, password=hashing.Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user/{id}', response_model=schemas.ShowUser)
+def getUser(id:int, db: Session = Depends(get_db)):
+    """
+    Retrive an individual user with a specific id
+    """
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User with the {'name'} not available")
+    return user
